6703445c2d3ef7ccc80aea75f6c35925e26b36e1
Rachata <paretk7@gmail.com>
2017-11-04 16:41:53 +0700

    Create APIs Stock firebase

--- index.js
+++ index.js
@@ -8,18 +8,74 @@ exports.getBalance = functions.https.onRequest((req, res) => {
   })
 })
 
-exports.withdraw = functions.https.onRequest((req, res) => {
+exports.withdrawTransaction = functions.https.onRequest((req, res) => {
   let withdrawAmount = req.query.amount
-  admin.database().ref('/balance').once('value').then(snapshot => {
-    let oldBalance = snapshot.val()
-    let newBalance = oldBalance - withdrawAmount
-    admin.database().ref('/balance').set(newBalance).then(() => {
+  let oldBalance = 0
+  admin.database().ref('/balance').transaction(currentBalance => {
+    if (currentBalance) {
+      oldBalance = currentBalance
+      return currentBalance - withdrawAmount
+    }
+    return currentBalance
+  }, (error, committed, snapshot) => {
+    if (error) {
+      res.status(500).send('Transaction failed abnormally!', error)
+    } else if (!committed) {
+      res.status(500).send('Aborted the transaction.')
+    } else {
+      let newBalance = snapshot.val()
       console.log({
         oldBalance,
         withdrawAmount,
         newBalance
       })
       res.send('success')
-    })
+    }
+  })
+})
+
+exports.stock = functions.https.onRequest((req, res) => {
+  let amount = parseInt(req.query.Amount)
+  let id = req.query.ProductId
+  let Actions = req.query.Actions
+  let inStock = 0
+  let output = 0
+  admin.database().ref('/products/' + id + '/inStock').transaction(currentStock => {
+    if (currentStock) {
+      inStock = currentStock
+      if(Actions === 'Deduct') {
+        if(parseInt(currentStock) >= amount){
+          output = inStock - amount
+          return output
+        }else {
+          res.status(403).send('Not enough values for Deduct')
+          return
+        }
+      }else if(Actions === 'Add') {
+        output = inStock + amount
+        return output
+      }else if (Actions === 'Update') {
+        output = amount
+        return output
+      }
+    }
+    return currentStock
+  }, (error, committed, snapshot) => {
+    if (error) {
+      res.status(403).send('Transaction failed abnormally!', error)
+    } else if (!committed) {
+      res.status(403).send('Aborted the transaction.')
+    } else {
+      let newStock = snapshot.val()
+      console.log({
+        inStock,
+        amount,
+        Actions,
+        newStock,
+        id,
+        output
+      })
+      res.send(`${snapshot.val()}`)
+    }
   })
 })
